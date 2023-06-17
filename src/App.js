31d576a4b7c313c332469cf22cac502f9d5927b1
Pedro Henrique <101227996+Ponze96@users.noreply.github.com>
2023-06-17 17:21:31 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,60 +1,71 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
+// MY IMPORTS
+import './style.css';
+
+
 function App() {
+
   const [texto, setTexto] = useState('');
   const [resposta, setResposta] = useState('');
 
-  function generateText(){
+  function generateText() {
     const prompt = texto;
-    const model = 'text-davinc-002';
+    const model = 'text-davinci-002';
     const maxTokens = 2048;
-    
-    const  REACT_APP_OPENAI_API_KEY = "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";
 
+    const REACT_APP_OPENAI_API_KEY = "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";
+    
     axios.post('https://api.openai.com/v1/completions', {
       prompt: prompt,
       model: model,
-      max_tokes: maxTokens
+      max_tokens: maxTokens
     }, {
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer ${REACT_APP_OPENAI_API_KEY}'
+        'Authorization': `Bearer ${REACT_APP_OPENAI_API_KEY}`
       }
     })
-      .then(response => {
-        console.log(response.data.choices[0].text);
-        setResposta(response.data.choices[0].text);
-        setTexto('');
-      })
-      .catch(error => {
-        console.error(error);
-        setResposta(error);
-      });
+    .then(response => {
+      console.log(response.data.choices[0].text);
+      setResposta(response.data.choices[0].text);
+      setTexto('');
+    })
+    .catch(error => {
+      console.error(error);
+      setResposta(error);
+    });
   }
-    const captureAudioClick = () => {
-      const reconhecimento = new window.webkitSpeenchRecognition();
 
-      reconhecimento.onresult = (event) =>{
-        const resultado = event.results[event.resultIndex];
-        setTexto(resultado[0].transcript);
-      };
-      
-      reconhecimento.start();
+  const captureAudioClick = () => {
+    const reconhecimento = new window.webkitSpeechRecognition();
+
+    reconhecimento.onresult = (event) => {
+      const resultado = event.results[event.resultIndex];
+      setTexto(resultado[0].transcript);
     };
+
+    reconhecimento.start();
+  };
+
+  
 
   return (
     <div className="container">
-      <div className='textos-container'>
-        <input type='text' placeholder='Escreva...' className='pergunta' onChange={((e) => setTexto(e.target.value))} value={texto} />
-        <textarea value={resposta} rows="10" disabled placeholder='Resposta da IA' className='Resposta' />
-      </div>
-      <div className='btn-container'>
-        <button onClick={captureAudioClick}>Transformar em texto</button>
-        <button type='button' onClick={generateText}>Pergunta</button>
-      </div>
+        <div className='textos-container'>
+          <input type="text" placeholder='Escreva...' className='pergunta' onChange={((e) => setTexto(e.target.value))} value={texto}/>
+
+          <textarea value={resposta} rows="10" disabled placeholder='Resposta da IA' className='resposta'/>
+        </div>
+
+        <div className='btn-container'>
+          <button onClick={captureAudioClick}>Transformar em Texto</button>
+          <button type='button' onClick={generateText}>Perguta</button>
+        </div>
     </div>
   );
 }
 
 export default App;
+
